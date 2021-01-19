754524903fa625781a79c5b1a92285d0b4e8b5f5
Miami-stack <muratajmedov@gmail.com>
2021-01-19 20:33:13 +0500

    dssdsd

--- main.py
+++ main.py
@@ -6,14 +6,7 @@ import random
 from typing import Dict, Any
 
 
-def false_input(values: list) -> int:
-    """Проверка введенного значения с клавиатуры."""
-    while True:
-        try:
-            value = int(input())
-        except ValueError:
-            print("Надо ввести 1 или 2 ")
-            continue
+
 
 
 class Item(ABC):
@@ -218,7 +211,213 @@ def var_spawner():
     SPAWNER_TYPE = random.choice(enemy_type_list)
     spawner = spawner_to_factory_mapping[SPAWNER_TYPE]()
     enemy = spawner.create_enemy()
-    print(f'Вы встретили врага {enemy.enemy_type}, с жизнями {enemy.hp} и атакой {enemy.damage}')
+    print(f'Вы встретили врага {enemy.var_type}, с жизнями {enemy.hp} и атакой {enemy.damage}')
+    return enemy
+
+class Hero(ABC):
+    """Абстрактный класс для героя."""
+    name = ''
+    hp = 30
+    weapons_list = {'sword': 10}
+    orcs_dead = 0
+    hero_type = ''
+
+    def get_weapon(self, weapon):
+            """Подобрать оружие."""
+            while True:
+                choice = input('Введите 1 ,чтобы поднять оружие или 2, чтобы пройти мимо : ')
+                if choice == '1':
+                    new_weapons_list = self.weapons_list
+                    new_weapons_list[weapon[0]] = weapon[1]
+                    self.weapons_list = new_weapons_list
+                    break
+                elif choice == '2':
+                    print('Вы прошли мимо')
+                    break
+
+    def hero_attack(self, weapon_list=weapons_list):
+        """Атака героя."""
+        temp = {}
+        for i in range(len(weapon_list)):
+            print(f'Нажмите {i + 1}, чтобы выбрать {list(weapon_list)[i]}')
+            temp[i + 1] = list(weapon_list)[i]
+        choice = input('Введите номер оружия: ')
+        while True:
+            if choice:
+                print(f'Вы выбрали {temp[int(choice)]}, ваш инвентарь {weapon_list[temp[int(choice)]]}')
+                return weapon_list[temp[int(choice)]]
+            else:
+                print('У вас нет оружия')
+
+    @abstractmethod
+    def special_class_skill(self):
+        block_attack = random.choice([True, False])
+        pass
+
+
+class Warrior(Hero):
+    """Класс воин."""
+
+    def special_class_skill(self, enemy_attack: tuple):
+        if enemy_attack[1] == 'warrior':
+            luck = random.choice([True, False])
+            if luck == True:
+                print('Вы заблокировали атаку ближнего боя')
+                return True
+            else:
+                print('Вы не смогли заблокировать атаку')
+                return enemy_attack[0]
+
+
+
+
+class Archer(Hero):
+    """Класс лучник."""
+
+    def special_class_skill(self, enemy_attack: tuple):
+        if enemy_attack[1] == 'archer':
+            luck = random.choice([True, False])
+            if luck == True:
+                print('Вы заблокировали атаку дальнего боя')
+                return True
+            else:
+                print('Вы не смогли заблокировать атаку')
+                return enemy_attack[0]
+
+
+
+class Mag(Hero):
+    """Класс мага."""
+
+    def special_class_skill(self, enemy_attack: tuple):
+        if enemy_attack[1] == 'mage':
+            luck = random.choice([True, False])
+            if luck == True:
+                print('Вы заблокировали магическую атаку')
+                return True
+            else:
+                print('Вы не смогли заблокировать атаку')
+                return enemy_attack[0]
+
+
+
+class EnemyFactory(ABC):
+    """Абстрактная фабрика для героя."""
+
+    @abstractmethod
+    def create_enemy(self):
+        """Создание абстрактного продукта ."""
+        pass
+
+
+class WarriorHeroFactory(EnemyFactory):
+    """Конкретная фабрика для героя."""
+
+    def create_enemy(self):
+        return Warrior()
+
+
+class ArcherHeroFactory(EnemyFactory):
+    """Конкретная фабрика для героя."""
+
+    def create_enemy(self):
+        return Archer()
+
+
+class MagHeroFactory(EnemyFactory):
+    """Конкретная фабрика для врага."""
+
+    def create_enemy(self):
+        return Mag()
+
+def hero_spawner_warriror():
+    spawner_to_factory_mapping = {"warriror": WarriorHeroFactory}
+    spawner = spawner_to_factory_mapping["warriror"]()
+    enemy = spawner.create_enemy()
+    return enemy
+
+def hero_spawner_archer():
+    spawner_to_factory_mapping = {"archer": ArcherHeroFactory}
+    spawner = spawner_to_factory_mapping["archer"]()
+    enemy = spawner.create_enemy()
+    return enemy
+
+def hero_spawner_mag():
+    spawner_to_factory_mapping = {"mag": ArcherHeroFactory}
+    spawner = spawner_to_factory_mapping["mag"]()
+    enemy = spawner.create_enemy()
     return enemy
 
 
+
+while True:
+    type_selector = input('Выберите класс вашего героя . 1 - воин  , 2 - лучник , 3 - маг : ')
+    if type_selector == '1':
+        hero = hero_spawner_warriror()
+        break
+    elif type_selector == '2':
+        hero = hero_spawner_archer()
+        break
+    elif type_selector == '3':
+        hero = hero_spawner_mag()
+        break
+    print(hero)
+
+dead = False
+while hero.orcs_dead != 10:
+    random_event = random.randint(1, 2)
+    if random_event == 1:
+        '''Битва с монстром'''
+        enemy = var_spawner()
+        while True:
+            choice = input('Введите 1, чтобы драться или 2, чтобы убежать : ')
+            if choice == '1':
+                count = 1
+                while True:
+                    enemy.hp = enemy.hp - hero.hero_attack()
+                    if enemy.hp <= 0:
+                        print('У монстра осталось - 0 жизней')
+                        print('Монстр побежден')
+                        hero.hp = hero.hp - hero.special_class_skill(enemy.attack())
+                        hero.monster_dead += 1
+                        break
+                    if hero.hp <= 0:
+                        print('')
+                        print('Вы умерли')
+                        dead = True
+                        break
+                    count += 1
+                    print(f'У вас осталось - {hero.hp} жизней')
+                break
+            elif choice == '2':
+                print('Вы сбежали')
+                break
+
+    elif random_event == 2:
+        item = item_spawner()
+        if item.name in ('sword', 'bow', 'magicbook'):
+            print(f'Вы обнаружили {item.name} с силой - {item.attack_power}')
+            hero.get_weapon(item.attack())
+            print('Ваш инвентарь - ' +  str(hero.weapons_list))
+        elif item.name == 'arrows':
+            print('Вы обнаружили стрелы')
+            hero.summary_arrows += item.attack()
+        elif item.name == 'apple':
+            print('ВЫ нашли яблоко')
+            hero.hp += item.heal()
+            print("Кол-во жизней : " + str(hero.hp))
+
+            while True:
+                choice = input('Введите 1 ,чтобы сохранится '
+                               'или 2, чтобы пройти мимо : ')
+                if choice == '1':
+                    sleep(1)
+                    hero.save_game()
+                    print('Игра сохранена. Здоровье {}, Оружие {}'.format(hero.hp, hero.weapons_list))
+                    break
+                elif choice == '2':
+                    print('Вы прошли мимо')
+                    break
+                else:
+                    print('Вы ввели неверное значение. Введите 1 или 2')
+    print(f'Орков убито {hero.orcs_dead}')
